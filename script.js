67934e4e74d6847e9001c4e40ca98ee1ba3d2a5d
e236314 <e236314@edu.sbl.ch>
2023-11-30 11:23:38 +0100

    Aufgabe 4 gelöst

--- script.js
+++ script.js
@@ -15,4 +15,56 @@ export function aufgabe01(args) {
 export function aufgabe02(args) {
   const input = args 
   return input.toUpperCase();
+
+}
+
+export function aufgabe03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input [i]
+    if (currentElement ==="e" || currentElement ==="E") {
+      result++
+    }
+  }
+  return result
+}
+
+export function aufgabe04(args) {
+     
+  const cleanText = args.replace(/[^\w\s]/gi, '');
+  const words = cleanText.split(/\s+/);
+
+  return words.length;
+}
+
+export function aufgabe05(args) {
+  const input = args
+  if (input.toLowerCase() === input) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function aufgabe06(args) {
+  const input = args
+  if (input === "") {
+    return false
+  }
+  for ( let i =0; i < input.length; i++) {
+    const currentElement = input [i].toLowerCase()
+    if (currentElement.charCodeAt() < 97 || currentElement.charCodeAt() > 122) {
+      if (currentElement.charCodeAt() != 32 ){
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+export function aufgabe07(args) {
+
 }
